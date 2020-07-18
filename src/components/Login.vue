d4b3c2bde28e770254ff8ce18ef270678771c3e3
nyaxs <nyaxs@outlook.com>
2020-07-18 21:16:12 +0800

    --

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="login">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  props: {
+    name:String,
+    password:String
+  }
+}
+</script>
+
+<style>
+#login {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
